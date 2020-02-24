a03a915c262dc2533f9216375d8b0d547928b962
宮城孝明 <miyagitakaaki@miyagitakaakinoMacBook-Pro.local>
2020-02-24 18:54:09 +0900

    First commit

---
+++ Test/sample.c
@@ -0,0 +1,20 @@
+#include　<stdio.h>
+
+int main(){
+    int get_int;
+
+    printf("数字を入力してください");
+    scanf("%d", &get_int);
+
+    if (get_int < 1000) printf("入力値は1000より小さい");
+    else
+    {
+        if (get_int == 10000) printf("入力値は1000です");
+        else
+        {
+            printf("入力値は1000より高いです");
+        }
+        
+    }
+    return 0;
+}
